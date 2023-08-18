4e9d020f1dc390fa605c189470799830b6fa757a
Dreyrk <lucas.rondepierre123@gmail.com>
2023-08-18 11:04:16 +0200

    fix ci 3

--- tests/data.test.js
+++ tests/data.test.js
@@ -6,9 +6,6 @@ import main from "../db.js";
 import { manhwaSeed } from "../seed.js";
 
 describe("DATA", () => {
-  beforeEach(async () => {
-    await manhwaSeed();
-  });
   describe("Manhwa", () => {
     it("GET /api/manhwa/all should return an array of 38 manhwas", async () => {
       const res = await fetch("http://127.0.0.1:5000/api/manhwa/all");
