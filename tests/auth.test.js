f183becd56fe56c38c3364694904c75da9fc4793
Dreyrk <lucas.rondepierre123@gmail.com>
2023-08-19 15:28:57 +0200

    add auth tests

---
+++ tests/auth.test.js
@@ -0,0 +1,31 @@
+import chai from "chai";
+const { expect: chaiExcept } = chai;
+
+describe("AUTH", () => {
+  describe("Login/Register", () => {
+    it("Can Login when new account registered", async () => {
+      const newUser = {
+        pseudo: "Test User",
+        email: "test@email.com",
+        password: "test",
+      };
+      const opts = {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      };
+
+      await fetch("http://127.0.0.1:5000/api/users/register", opts);
+
+      const res = await fetch("http://127.0.0.1:5000/api/users/login", opts);
+
+      const data = await res.json();
+
+      chaiExcept(data.user.email).to.equals(newUser.email);
+      chaiExcept(data.token).to.exist;
+    });
+  });
+});
