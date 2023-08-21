f8c9c216f9e00bbaaa0318c6ffd3808d22773275
Dreyrk <lucas.rondepierre123@gmail.com>
2023-08-21 21:06:31 +0200

    modify tests

--- tests/data.test.js
+++ tests/data.test.js
@@ -3,11 +3,6 @@ const { expect: chaiExcept } = chai;
 
 describe("DATA", () => {
   describe("Manhwa", () => {
-    it("GET /api/manhwa/all should return an array of 19 manhwas", async () => {
-      const res = await fetch("http://127.0.0.1:5000/api/manhwa/all");
-      const data = await res.json();
-      chaiExcept(data.data.length).to.equal(19);
-    });
     it("GET /api/manhwa/id/:id should return manhwa with correct id", async () => {
       let testManhwa = await fetch("http://127.0.0.1:5000/api/manhwa/random");
       testManhwa = await testManhwa.json();
