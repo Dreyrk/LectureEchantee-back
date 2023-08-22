1d62259c88b8412bcf8eecf330cbe81a06fe3feb
Dreyrk <lucas.rondepierre123@gmail.com>
2023-08-23 00:18:41 +0200

    dodo

--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -4,16 +4,15 @@ dotenv.config();
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.header("Authorization");
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .json({ error: "Access denied. Invalid token format." });
-  }
-
-  const token = authHeader.substring(7); // Remove "Bearer " from the beginning
-
   try {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ error: "Access denied. Invalid token format." });
+    }
+
+    const token = authHeader.substring(7); // Remove "Bearer " from the beginning
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded.isAdmin) {
       req.admin = true;
